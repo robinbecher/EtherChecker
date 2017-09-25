9b4e98db19ea7b4a4b1e624bdc1f60e8a771f17c
Robin Becher <robin.becher@gmail.com>
2017-09-25 15:25:48 +0200

    new GUI implementation, yet to finish

---
+++ EtherChecker/src/code/EtherCheckerGUI.java
@@ -0,0 +1,10 @@
+package code;
+
+import javax.swing.*;
+
+public class EtherCheckerGUI {
+    private JPanel priceInfoPanel;
+    private JPanel titlePanel;
+    private JPanel generalInfoPanel;
+    private JCheckBox alwaysOnTopCheckBox;
+}
